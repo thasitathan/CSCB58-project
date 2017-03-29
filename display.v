f7e00592717ce13f65b26c880030f4ca52f0b26d
thasisivakumaran <thasi.sivakumaran@mail.utoronto.ca>
2017-03-29 14:38:48 -0400

    Update display.v

--- display.v
+++ display.v
@@ -155,3 +155,549 @@ module display
 			speed2 <= 1'b1;
 			/////
 			
+			
+		end
+		else if(blackOut) begin
+			colour2 <= 3'b000;
+			blackOut <= 1'b0;
+		end
+		
+		else if(!KEY[1] || rightMove)begin
+			if(a == 8'd80) begin
+				
+				if(rightMove)begin
+					colour2 <= 3'b001;
+					a <= 8'd130;
+					rightMove <= 1'b0;
+				end
+				else begin
+					blackOut <= 1'b1;
+					rightMove <= 1'b1;
+					drawPlayer
+				end
+			end
+			else if(a == 8'd30) begin
+				
+				if(rightMove)begin
+					colour2 <= 3'b001;
+					a <= 8'd80;
+					rightMove <= 1'b0;
+				end
+				else begin
+					blackOut <= 1'b1;
+					rightMove <= 1'b1;
+				end
+			end
+		end
+		
+		else if(!KEY[3] || leftMove)begin
+			if(a == 8'd80) begin
+				
+				if(leftMove)begin
+					colour2 <= 3'b001;
+					a <= 8'd30;
+					leftMove <= 1'b0;
+				end
+				else begin
+					blackOut <= 1'b1;
+					leftMove <= 1'b1;
+				end
+
+			end
+			else if(a == 8'd130) begin
+				
+				if(leftMove)begin
+					colour2 <= 3'b001;
+					a <= 8'd80;
+					leftMove <= 1'b0;
+				end
+				else begin
+					
+					blackOut <= 1'b1;
+					leftMove <= 1'b1;
+				end
+			end
+		end
+	end
+	
+	
+	
+	*/
+	wire [27:0] rd3_out;
+	reg Enable;
+	wire [3:0] dc0_out;
+	
+	//RateDivider rd1(CLOCK_50, rd3_out, speed1, 1'b1, 28'b1011111010111100000111111111, 1);
+	always@(posedge CLOCK_50) begin	
+		//Enable = (rd3_out == 28'b0000000000000000000000000000) ? 1'b1 : 1'b0;
+		if (scoreCounter != 20'd500000) begin
+			scoreCounter <= scoreCounter + 20'd1;
+			end
+		else begin
+			
+			//score <= score + 16'd1;
+			scoreCounter <= 20'd0;
+			
+			if(score4 == 4'b1001)begin
+				score4Add <= 1'b0;
+				end
+			if(score3 == 4'b1001 && score4Add == 1'b0)begin
+				score3Add <= 1'b0;
+				end
+			if(score2 == 4'b1001 && score3Add == 1'b0 && score4Add == 1'b0)begin
+				score2Add <= 1'b0;
+				end
+			if(score1 == 4'b1001 && score2Add == 1'b0 && score3Add == 1'b0 && score4Add == 1'b0)begin
+				score1Add <= 1'b0;
+				score1 <= 4'b1001;
+				end
+			else if(score1Add)begin
+				score1 <= score1 + 4'd1;
+			end
+			
+			if(score1 == 4'b1010 && score2Add)begin
+				score2 <= score2 + 4'd1;
+				score1 <= 4'd0;
+				end
+			if(score2 == 4'b1010 && score3Add)begin
+				score3 <= score3 + 4'd1;
+				score2 <= 4'd0;
+				end
+			if(score3 == 4'b1010 && score4Add)begin
+				score4 <= score4 + 4'd1;
+				score3 <= 4'd0;
+				end
+		end
+		
+	end
+	//DisplayCounter dc0(CLOCK_50, dc0_out, 1'b1, Enable);
+	hex_decoder(score1,HEX0);
+	
+	hex_decoder(score2,HEX1);
+	
+	hex_decoder(score3,HEX2);
+	
+	hex_decoder(score4,HEX3);
+	
+	
+
+	
+	
+	
+	always@(posedge clock) begin
+		/*begin
+			RateDivider rd1(CLOCK_50, clock, speed1, 1'b1, 28'b1011111010111100000111111111, speed2);
+		end*/
+		if(out_y1 <= 7'd120) begin
+			plotSig <= 1'b1;
+			x <= 8'd53;
+			colour <= 3'b111;
+			out_y1 <= out_y1 + 7'b0000001;
+			y <= out_y1;
+		end
+		
+		else if(out_y2 <= 7'd120) begin
+			x <= 8'd106;
+			colour <= 3'b111;
+			out_y2 <= out_y2 + 7'b0000001;
+			y <= out_y2;
+		end
+		
+		
+		else if(plotPlayer == 1'b0) begin
+			y <= 7'd100;
+			x <= 8'd80;
+			colour <= 3'b001;
+			plotPlayer <= 1'b1;
+			speed1 <= 1'b1;
+			speed2 <= 1'b1;
+			
+		end
+		else if(blackOut) begin
+			
+			if(createCarCounter == 4'd0)begin
+				colour <= 3'b000;
+				x <= x + 0;
+				y <= y + 0;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			/*else if(createCarCounter == 4'd1)begin
+				colour <= 3'b000;
+				x <= x - 8'd1;
+				y <= y + 0;
+				createCarCounter <= createCarCounter + 4'd1;
+
+			end
+			else if(createCarCounter == 4'd2)begin
+				colour <= 3'b000;
+				x <= x + 8'd2;
+				y <= y + 0;
+				createCarCounter <= createCarCounter + 4'd1;
+
+			end
+			else if(createCarCounter == 4'd3)begin
+				colour <= 3'b000;
+				x <= x - 8'd3;
+				y <= y - 7'd1;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd4)begin
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd5)begin
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd6)begin
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd7)begin
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd8)begin
+				colour <= 3'b000;
+				x <= x - 8'd4;
+				y <= y + 7'd2;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd9)begincreateCarCounter <= 4'd0;
+				createCar <= 1'b0;
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd10)begin
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd11)begin
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd12)begin
+				colour <= 3'b000;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= creassteCarCounter + 4'd1;
+			end*/
+			else begin
+				createCarCounter <= 4'd0;
+				blackOut <= 1'b0;
+			end
+		end
+		
+		else if(createCar == 1'b1) begin
+			if(createCarCounter == 4'd0)begin
+				colour <= 3'b001;
+				x <= x + 0;
+				y <= y + 0;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			/*else if(createCarCounter == 4'd1)begin
+				colour <= 3'b001;
+				x <= x - 8'd1;
+				y <= y + 0;
+				createCarCounter <= createCarCounter + 4'd1;
+
+			end
+			else if(createCarCounter == 4'd2)begin
+				colour <= 3'b001;
+				x <= x + 8'd2;
+				y <= y + 0;
+				createCarCounter <= createCarCounter + 4'd1;
+
+			end
+			else if(createCarCounter == 4'd3)begin
+				colour <= 3'b011;
+				x <= x - 8'd3;
+				y <= y - 7'd1;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd4)begin
+				colour <= 3'b001;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd5)begin
+				colour <= 3'b001;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd6)begin
+				colour <= 3'b001;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd7)begin
+				colour <= 3'b011;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd8)begin
+				colour <= 3'b011;
+				x <= x - 8'd4;
+				y <= y + 7'd2;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd9)begin
+				colour <= 3'b001;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd10)begin
+				colour <= 3'b001;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd11)begin
+				colour <= 3'b001;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= createCarCounter + 4'd1;
+			end
+			else if(createCarCounter == 4'd12)begin
+				colour <= 3'b011;
+				x <= x + 8'd1;
+				y <= y;
+				createCarCounter <= creassteCarCounter + 4'd1;
+			end*/
+			else begin
+				createCarCounter <= 4'd0;
+				createCar <= 1'b0;
+			end
+		end
+		
+		else if(!KEY[1] || rightMove)begin
+			if(rightMove)begin
+				colour <= 3'b001;
+				x <= 8'd130;
+				rightMove <= 1'b0;
+				createCar <= 1'b1;
+				createCarCounter <= 4'd0;
+			end
+			else begin
+				blackOut <= 1'b1;
+				rightMove <= 1'b1;
+				createCarCounter <=4'd0;
+			end
+			/*
+			if(x == 8'd80) begin
+				
+				if(rightMove)begin
+					colour <= 3'b001;
+					x <= 8'd130;
+					rightMove <= 1'b0;
+					createCar <= 1'b1;
+				end
+				else begin
+					blackOut <= 1'b1;
+					rightMove <= 1'b1;
+				end
+			end
+			else if(x == 8'd30) begin
+				
+				if(rightMove)begin
+					colour <= 3'b001;
+					x <= 8'd80;
+					rightMove <= 1'b0;
+					createCar <= 1'b1;
+				end
+				else begin
+					blackOut <= 1'b1;
+					rightMove <= 1'b1;
+				end
+			end*/
+		end
+		
+		else if(!KEY[3] || leftMove)begin
+			if(leftMove)begin
+				colour <= 3'b001;
+				x <= 8'd30;
+				leftMove <= 1'b0;
+				createCar <= 1'b1;
+				createCarCounter <=4'd0;
+			end
+			else begin
+				blackOut <= 1'b1;
+				leftMove <= 1'b1;
+				createCarCounter <=4'd0;
+			end
+
+			
+			/*if(x == 8'd80) begin
+				
+				if(leftMove)begin
+					colour <= 3'b001;
+					x <= 8'd30;
+					leftMove <= 1'b0;
+					createCar <= 1'b1;
+				end
+				else begin
+					blackOut <= 1'b1;
+					leftMove <= 1'b1;
+				end
+
+			end
+			else if(x == 8'd130) begin
+				
+				if(leftMove)begin
+					colour <= 3'b001;
+					x <= 8'd80;
+					leftMove <= 1'b0;
+					createCar <= 1'b1;
+				end
+				else begin
+					
+					blackOut <= 1'b1;
+					leftMove <= 1'b1;
+				end
+			end*/
+		end
+		
+		else if(!KEY[2] || middleMove)begin
+			if(middleMove)begin
+				colour <= 3'b001;
+				x <= 8'd30;
+				middleMove <= 1'b0;
+				createCar <= 1'b1;
+				createCarCounter <=4'd0;
+			end
+			else begin
+				blackOut <= 1'b1;
+				middleMove <= 1'b1;
+				createCarCounter <=4'd0;
+			end
+		end
+		
+		
+	end
+endmodule
+
+
+
+module datapath(
+	input clk3,
+	input clk1,
+	input resetn,
+   //input ld_x, ld_y, ld_colour,
+   input [9:0] data_in,
+	output reg[2:0]  out_colour,
+   output reg[7:0]  out_x,
+	output reg[6:0]  out_y
+   );
+    
+   // output of the alu//////////////////////
+   //assign out_x 	 	= 7'b0000000;
+	//assign out_y 	 	= 6'b000000;
+	//assign out_colour 	= 3'b000;
+   	
+	// Register x  is set with respective input logic
+   always@(posedge clk3) begin
+		if(!resetn) begin
+			out_x 		<= 8'b00000000;
+		end
+		else begin
+			out_x <= {1'b0, data_in[6:0]};
+		end
+	end
+
+   // Register y and colour are set with respective input logic
+   always@(posedge clk1) begin
+		if(!resetn) begin
+			out_y 		<= 7'b0000000;
+			out_colour 	<= 3'b000;
+		end
+		else begin
+			out_y 		<= data_in[6:0];
+			out_colour 	<= data_in[9:7];
+		end
+	end
+endmodule
+
+module control(
+	input clk,
+   input resetn,
+   input [7:0] x,
+	input [6:0] y,
+	input [2:0] colour,
+	output reg[2:0]  ld_alu_out_colour1,
+	output reg[2:0]  ld_alu_out_colour2,
+   output reg[7:0]  ld_alu_out_x,
+	output reg[6:0]  ld_alu_out_y
+    );
+
+    // Output logic aka all of our datapath control signals
+	// By default make all our signals 0
+	/////////////////////////////////////////////
+	//assign ld_alu_out_x = 2'b0;
+	//assign ld_alu_out_y = 2'b0;
+	//assign ld_alu_out_colour = 2'b0;
+   
+    // current_state registers
+    always@(posedge clk)
+    begin: state_FFs
+        if(!resetn)
+			begin
+				ld_alu_out_x <= 8'b00000000;
+				ld_alu_out_y <= 7'b0000000;
+				ld_alu_out_colour1 <= 3'b000;
+				ld_alu_out_colour2 <= ld_alu_out_colour1;
+			end
+        else
+            begin
+				ld_alu_out_x <= {1'b0,x};
+				ld_alu_out_y <= y;
+				ld_alu_out_colour1 <= colour;
+				ld_alu_out_colour2 <= ld_alu_out_colour1;
+			end
+    end // state_FFS
+endmodule
+
+
+module hex_decoder(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
+endmodule
